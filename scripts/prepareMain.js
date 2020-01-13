cac7ee0e3e05e87d523ef801ed6f2c66f81fdeb5
Christopher J. Brody <chris@brody.consulting>
2020-01-13 12:40:01 -0500

    move index-*.js scripts into src/browser

--- scripts/prepareMain.js
+++ scripts/prepareMain.js
@@ -1,15 +1,15 @@
 const concat = require('concat')
 
 concat([
-  'index-header.js',
-  'index-mod1-header.js',
+  'src/browser/index-header.js',
+  'src/browser/index-mod1-header.js',
   'src/deps/browser/sql-memory-growth.js',
-  'index-mod1-footer.js',
-  'index-mod2-header.js',
+  'src/browser/index-mod1-footer.js',
+  'src/browser/index-mod2-header.js',
   'src/browser/SQLiteProxy.js',
-  'index-mod2-footer.js',
-  'index-mod3-header.js',
+  'src/browser/index-mod2-footer.js',
+  'src/browser/index-mod3-header.js',
   'www/SQLitePlugin.js',
-  'index-mod3-footer.js',
-  'index-footer.js'
+  'src/browser/index-mod3-footer.js',
+  'src/browser/index-footer.js'
 ], 'index.js')
